6045f4446b0f32b7b99a5551f4c97c1129de6ea6
unknown <allblue312@gmail.com>
2020-12-10 12:44:59 +0300

    test

--- src/test/java/testproject/AggregatorTest.java
+++ src/test/java/testproject/AggregatorTest.java
@@ -27,4 +27,13 @@ public class AggregatorTest {
         agr.addValue(val);
         assertThat(agr.getSum()).isEqualTo(val * 2);
     }
+
+
+@Test
+public void testReset() {
+Aggregator agr = new Aggregator();
+agr.addValue(10);
+agr.reset();
+assertThat(agr.getSum()).isEquals(0);
+}
 }
